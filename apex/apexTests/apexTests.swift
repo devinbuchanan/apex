9cb29d295f24e663221a7cf17b3a397f4b3d31de
noideawhatimdoing-llm <bigdevinb@icloud.com>
2025-06-13 21:59:30 -0700

    initial xcode files

---
+++ apex/apexTests/apexTests.swift
@@ -0,0 +1,17 @@
+//
+//  apexTests.swift
+//  apexTests
+//
+//  Created by Devin Buchanan on 6/13/25.
+//
+
+import Testing
+@testable import apex
+
+struct apexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
